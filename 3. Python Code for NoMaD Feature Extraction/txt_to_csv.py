6e9358d026e8dc8d1707a80cdb864c298128df08
FAZnF3 <47087481+FAZnF3@users.noreply.github.com>
2019-02-09 13:20:42 -0500

    Update txt_to_csv.py

--- 3. Python Code for NoMaD Feature Extraction/txt_to_csv.py
+++ 3. Python Code for NoMaD Feature Extraction/txt_to_csv.py
@@ -2,7 +2,10 @@ import csv
 import os
 
 # convert txt to csv file
-header = ["Name", "Cation_formula", "Num_Atom_Types", "Num_Atoms", "Atom_Types", "Bandgap_GGA_(eV)", "Dielectric_Constant_electronic", "Dielectric_constant_ionic", "Dielectric_constant_total", "Refractive_index", "Atomization_Energy(eV/atom)", "Relative_energy1(eV/atom)", "Relative energy2(eV/atom)", "Unit_cell_volume_(A^3)", "Density_(g/cm^3)", "Tool", "Pseudopotential", "Note"]
+header = ["Name", "Cation_formula", "Num_Atom_Types", "Num_Atoms", "Atom_Types", "Bandgap_GGA_(eV)", 
+          "Dielectric_Constant_electronic", "Dielectric_constant_ionic", "Dielectric_constant_total", 
+          "Refractive_index", "Atomization_Energy(eV/atom)", "Relative_energy1(eV/atom)", "Relative energy2(eV/atom)", 
+          "Unit_cell_volume_(A^3)", "Density_(g/cm^3)", "Tool", "Pseudopotential", "Note"]
 with open('nomad_data.csv', 'w', newline='') as f_output:
    csv_output = csv.writer(f_output)
    csv_output.writerow(header)
